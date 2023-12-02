a8c457ba4649db5946e4a24fe60c7fadb1725d4f
Nick S <122714211+Zeexon@users.noreply.github.com>
2023-12-03 08:46:07 +1000

    Add files via upload

---
+++ for_maks_nikee_proj/script.js
@@ -0,0 +1,23 @@
+
+// let product_title = document.querySelector(".product-title")
+// let product_discription = document.querySelector(".product-discription")
+// product_title.innerText = products.map((item)=> item.title)
+// const products = [
+//     {
+//         id:1,
+//         price:15000,
+//         title: "snek"
+//     }, 
+
+//     {
+//         id:2,
+//         price:8000,
+//         title: "rob"
+//     },
+
+//     {
+//         id:3,
+//         price:12000,
+//         title: "mam"
+//     },
+// ]
